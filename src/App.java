8fc21f53650764ee9263f2f163a5de7087efbc90
luctamak001 <luctamak001@warren.k12.in.us>
2025-11-03 08:37:19 -0500

    Fix student and teacher in main

--- src/App.java
+++ src/App.java
@@ -2,11 +2,13 @@ public class App {
     public static void main(String[] args) {
 
         Person teacher1 = new Teacher("Miss. Michel", 28, "Math");
+        Person student1 = new Student("Kumar", 16, 11);
+        
+
+        System.out.println(¨Teacher Introduction:¨);
         teacher1.introduce();
 
-        System.out.println();
-
-        Person student1 = new Student("Kumar", 16, 11);
+        System.out.println(¨\nStudent Introduction:¨);
         student1.introduce();
     }
 }
